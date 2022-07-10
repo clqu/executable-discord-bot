b9b277ad90da70cebc6e24eba64acf594e7be990
clqu <76551996+clqu@users.noreply.github.com>
2022-07-11 00:43:41 +0300

    Delete ping.js

--- src/client/commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Pong...",
-    options: [],
-    permissions: [],
-    run: async (client, interaction) => {
-        return interaction.reply(`Pong... ${client.ws.ping}ms`);
-    }
-}
